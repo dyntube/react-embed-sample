17e3ba78808952793da85b02381d263cf4bb0aec
dyntube <dev@dyntube.com>
2022-07-16 15:41:33 +1000

    changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="player">
+      {/* Please note that the Javascript code was added to the <head> of index.html */}
+      {/* channelKey of your video as data-dyntube-key */}
+      <div data-dyntube-key="7Fav6D8nSUKEypztWUTwVQ"></div>
+    </div>
+  );
+}
+
+export default App;
